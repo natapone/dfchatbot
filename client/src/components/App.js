13b4136fcbc56728ac317b42211466f08dd87366
Natapone Charsombut <natapone@gmail.com>
2019-11-07 11:27:30 +0630

    split template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+
 import logo from '../logo.svg';
 import '../App.css';
+
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Shop from './pages/Shop';
 
 const App = () => {
   return (
---
+++ client/src/components/shop/itemList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemList = () => {
+  return (
+    <div>
+      Hear goes a list of items
+    </div>
+  )
+};
+
+export default ItemList;
