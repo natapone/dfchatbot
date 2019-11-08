e643fcf8ab6e50e46af4fdf82a4397142b4684bb
Natapone Charsombut <natapone@gmail.com>
2019-11-08 09:26:54 +0630

    apply materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,7 +13,7 @@ import Chatbot from './chatbot/Chatbot';
 const App = () => (
   <div>
     <BrowserRouter>
-      <div>
+      <div className="container">
         <Header/>
         <Route exact path="/" component={Landing} />
         <Route exact path="/about" component={About} />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return (
+const Header = () => (
     <nav>
-      <ul>
-        <li><Link to={'/shop'}>Shop</Link></li>
-        <li><Link to={'/about'}>About us</Link></li>
-      </ul>
+      <div className="nav-wrapper">
+        <Link to={'/'} class>IT courses</Link>
+        <ul>
+          <li><Link to={'/shop'}>Shop</Link></li>
+          <li><Link to={'/about'}>About us</Link></li>
+        </ul>
+      </div>
     </nav>
   )
-}
 
 export default Header;
