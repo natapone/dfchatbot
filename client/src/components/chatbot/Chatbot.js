fba67ddf1855ac3902401157ffd37fa173d102b7
Natapone Charsombut <natapone@gmail.com>
2019-11-08 13:55:29 +0630

    fix typo

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,6 +1,52 @@
 import React, {Component} from 'react';
+import axios from "axios/index";
 
 class Chatbot extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: []
+    }
+  }
+  async df_text_query (text) {
+    let says = {
+      speaks: 'me',
+      msg: {
+        text: {
+          text: {
+            text: text
+          }
+        }
+      }
+    };
+
+    this.setState({messages: [...this.state.messages, says]});
+    const res = await axios.post('/api/df_text_query', {text});
+
+    for (let msg of res.data.fulfillmentMessages) {
+      says = {
+        speaks: 'bot',
+        msg: msg
+      }
+      this.setState({ messages: [...this.state.messages, says]});
+    }
+  }
+
+  async df_event_query (event) {
+    const res = await axios.post('/api/df_event_query', {event});
+
+    for (let msg of res.data.fulfillmentMessages) {
+      let says = {
+        speaks: 'me',
+        msg: msg
+      };
+      this.setState({ messages: [...this.state.messages, says]});
+    }
+  }
+
+
   render() {
     return (
       <div style={{ height: 400, width: 400, float: 'right' }}>
